a8a2e2996f2d1ab0e8f7b76e5794a5fd0cc9c47c
cheonuk <cjsdnr885@naver.com>
2025-03-06 13:51:43 +0900

    invitation_v1.0

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ export const WEDDING_DATE = "2025년 07월 12일, 토요일 오후 2시";
 export const WEDDING_LOCATION = "여의도 루나미엘레, 12층 컨벤션홀";
 
 export const GROOM_NAME = "박천욱";
-export const GROOM_ACCOUNT_NUMBER = "국민은행 251802-04-108720";
+export const GROOM_ACCOUNT_NUMBER = "토스뱅크 1000-2380-2243";
 export const GROOM_FATHER_NAME = "박삼우";
-export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "농협은행 302-0139-6899-11";
 export const GROOM_MOTHER_NAME = "임미경";
-export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "국민은행 650702-01-057190";
 
 export const BRIDE_NAME = "배진영";
-export const BRIDE_ACCOUNT_NUMBER = "농협은행 123-456-789";
+export const BRIDE_ACCOUNT_NUMBER = "카카오뱅크 3333108119885";
 export const BRIDE_FATHER_NAME = "배명일";
-export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "카카오뱅크 3333103131866";
 export const BRIDE_MOTHER_NAME = "이남교";
-export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "카카오뱅크 3333119036178";
